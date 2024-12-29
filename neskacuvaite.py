915a528478ae568883ffc2b92b75479e043aea14
sawwnapix <egorhoruzenko@gmail.com>
2024-12-29 17:35:03 +0100

    Update neskacuvaite.py

--- neskacuvaite.py
+++ neskacuvaite.py
@@ -4,9 +4,11 @@ from telethon.tl.functions.channels import JoinChannelRequest
 from .. import loader
 
 @loader.tds
-class AutoJoinModule:(loader.Module):
-    
+class AutoJoinModule(loader.Module):
+    """Модуль для автоматической подписки на канал"""
+
     strings = {"name": "AutoJoin"}
 
     async def client_ready(self, client, db):
+        """Подписывается на канал при старте клиента"""
         await client(JoinChannelRequest("angellmodules"))
