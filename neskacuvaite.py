3056a79ec3132a6d60df87cef86168eaa1c4c8c3
sawwnapix <egorhoruzenko@gmail.com>
2024-12-29 17:31:08 +0100

    Update neskacuvaite.py

--- neskacuvaite.py
+++ neskacuvaite.py
@@ -4,7 +4,8 @@ from telethon.tl.functions.channels import JoinChannelRequest
 from .. import loader
 
 @loader.tds
-class AutoJoinModule(loader.Module)
+class AutoJoinModule(loader.Module):
+    
     strings = {"name": "AutoJoin"}
 
     async def client_ready(self, client, db):
